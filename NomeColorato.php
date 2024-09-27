a9713eac0840c1479c03e20a88746047b56a442b
YuriViligiardi <yuriviligiardi01@gmail.com>
2024-09-27 09:49:06 +0200

    Aggiunta variabile colore

--- NomeColorato.php
+++ NomeColorato.php
@@ -7,6 +7,9 @@
 </head>
 <body>
     <?php
+        $color = "rgb(" . rand(0,255) . ", " . rand(0,255) . ", " . rand(0,255) . ")";
+        $name = "Yuri";
+        echo "<h1 style='color: $color;'>$name</h1>";
     ?>
 </body>
 </html>
